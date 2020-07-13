bd03806005e2ad398239282d25ca15633bb55810
adriano.frota.silva <adriano.frota.silva@avanade.com>
2020-07-12 22:29:04 -0300

    add Newtonsoft.Json add LambdaLogger.Log

--- NetCoreAWSLambdaAndCloudwatchEvent/Function.cs
+++ NetCoreAWSLambdaAndCloudwatchEvent/Function.cs
@@ -1,6 +1,8 @@
 using Amazon.Lambda.CloudWatchEvents;
 using Amazon.Lambda.Core;
+using Newtonsoft.Json;
 using System;
+using System.Data;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -11,6 +13,11 @@ namespace NetCoreAWSLambdaAndCloudwatchEvent
     {
         public string FunctionHandler(CloudWatchEvent<Object> ev, ILambdaContext context)
         {
+
+            var eventReceived = JsonConvert.SerializeObject(ev);
+
+            var datahora = DateTime.Now.ToString("dd'/'MM'/'yyyy HH:mm:ss");
+            LambdaLogger.Log($"Yes, logget at: {datahora}! Event received:{eventReceived}");
             return "ok";
         }
     }
